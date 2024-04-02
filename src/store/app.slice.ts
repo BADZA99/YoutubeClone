ec0ba35920b39dc1bebb3d4be98c7bd8fef02acd
papabn <pndiaye999@gmail.com>
2024-04-02 00:42:45 +0000

    Add @reduxjs/toolkit and react-redux and slice setup

---
+++ src/store/app.slice.ts
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface IAppState{
+    theme: "light" | "dark"
+}
+
+
+const initialState : IAppState = {
+theme: "dark"
+}
+
+export const appSlice=createSlice({
+    name: "app",
+    initialState,
+    reducers:{
+        changeTheme:(state)=>{
+            state.theme=state.theme==="dark"?"light":"dark"
+        }
+    }
+});
+
+export const {changeTheme} = appSlice.actions;        
